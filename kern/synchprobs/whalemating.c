d50a71add6d96b6809a61653f8a628b37a7968a2
Hooman Keshvari <hm.keshvari@gmail.com>
2024-11-09 12:05:18 -0500

    implemented whaltmating problem

--- kern/synchprobs/whalemating.c
+++ kern/synchprobs/whalemating.c
@@ -40,11 +40,40 @@
 #include <test.h>
 #include <synch.h>
 
+
+struct cv * malecv;
+struct cv * femalecv;
+struct cv * matchmakercv;
+struct lock * malelock;
+struct lock * femalelock;
+struct lock * matchmakerlock;
+struct lock * datalock;
+struct semaphore * male_done;
+struct semaphore * female_done;
+struct semaphore * female_can_end;
+struct semaphore * male_can_end;
+unsigned male_count;
+unsigned female_count;
+unsigned matchmaker_count;
 /*
  * Called by the driver during initialization.
  */
 
 void whalemating_init() {
+	malecv = cv_create("Male Wait");
+	femalecv = cv_create("Female Wait");
+	matchmakercv = cv_create("Matchmaker Wait");
+	malelock = lock_create("Male Lock");
+	femalelock = lock_create("Female Lock");
+	matchmakerlock = lock_create("Matchmaker Lock");
+	datalock = lock_create("data Lock");
+	male_done = sem_create("Male Done", 0);
+	female_done = sem_create("Female Done", 0);
+	male_can_end = sem_create("Male ed", 0);
+	female_can_end = sem_create("Female ed", 0);
+	male_count = 0;
+	female_count = 0;
+	matchmaker_count = 0;
 	return;
 }
 
@@ -65,6 +94,31 @@ male(uint32_t index)
 	 * Implement this function by calling male_start and male_end when
 	 * appropriate.
 	 */
+	
+	lock_acquire(malelock);
+	male_start(index);
+	lock_acquire(datalock);
+	male_count ++;
+	lock_release(datalock);
+	lock_acquire(matchmakerlock);
+	cv_signal(matchmakercv, matchmakerlock);
+	lock_release(matchmakerlock);
+	lock_acquire(datalock);
+	while( female_count == 0 || matchmaker_count == 0){
+		lock_release(datalock);
+		cv_wait(malecv, malelock);
+		lock_acquire(datalock);
+	}
+	kprintf("loopa\n");
+	lock_release(datalock);
+	V(male_done);
+	P(male_can_end);
+	lock_acquire(datalock);
+	male_count --;
+	lock_release(datalock);
+	male_end(index);
+	lock_release(malelock);
+	
 	return;
 }
 
@@ -76,6 +130,31 @@ female(uint32_t index)
 	 * Implement this function by calling female_start and female_end when
 	 * appropriate.
 	 */
+	
+	lock_acquire(femalelock);
+	female_start(index);
+	lock_acquire(datalock);
+	female_count ++;
+	lock_release(datalock);
+	lock_acquire(matchmakerlock);
+	cv_signal(matchmakercv, matchmakerlock);
+	lock_release(matchmakerlock);
+	lock_acquire(datalock);
+	while(male_count == 0 || matchmaker_count == 0){
+		lock_release(datalock);
+		cv_wait(femalecv, femalelock);
+		lock_acquire(datalock);
+	}
+	kprintf("doopa\n");
+	lock_release(datalock);
+	V(female_done);
+	P(female_can_end);
+	lock_acquire(datalock);
+	female_count --;
+	lock_release(datalock);
+	female_end(index);
+	lock_release(femalelock);
+	
 	return;
 }
 
@@ -87,5 +166,33 @@ matchmaker(uint32_t index)
 	 * Implement this function by calling matchmaker_start and matchmaker_end
 	 * when appropriate.
 	 */
+	matchmaker_start(index);
+	lock_acquire(matchmakerlock);
+	
+	lock_acquire(datalock);
+	while(male_count == 0 || female_count == 0){
+		lock_release(datalock);
+		cv_wait(matchmakercv, matchmakerlock);
+		lock_acquire(datalock);
+	}
+	kprintf("koopa\n");
+	matchmaker_count ++;
+	lock_release(datalock);
+	lock_acquire(malelock);
+	cv_signal(malecv, malelock);
+	lock_release(malelock);
+	lock_acquire(femalelock);
+	cv_signal(femalecv, femalelock);
+	lock_release(femalelock);
+	P(female_done);
+	P(male_done);
+	lock_acquire(datalock);
+	matchmaker_count --;
+	lock_release(datalock);
+	V(male_can_end);
+	V(female_can_end);
+	lock_release(matchmakerlock);
+	matchmaker_end(index);
+	
 	return;
 }
