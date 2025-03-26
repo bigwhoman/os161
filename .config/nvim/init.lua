c03fb7722019e45de7a56a57d1565429c2017da8
Hooman <hm.keshvari@gmail.com>
2025-03-26 09:09:23 -0400

    changed nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,4 +15,9 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true 
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 999
